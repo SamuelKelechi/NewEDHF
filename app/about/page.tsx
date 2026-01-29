9486c5bc87b7c5be9ee81e18ad3073cc861481c5
ALADETAN-IFE <famousfotune123@gmail.com>
2026-01-29 21:52:03 +0100

    feat(team): add Team and Member components with images
    Added Member and Team components for displaying team members
    Implemented inline background image styling for member photos
    Added team member images to public directory
    Updated about.module.css for team section and member card styles
    Updated about page and index exports for new components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,11 @@
-import { HeroSection, ValuesSection } from "@/components/about";
+import { HeroSection, Team, ValuesSection } from "@/components/about";
 
 export default function About() {
   return (
     <>
       <HeroSection />
       <ValuesSection />
+      <Team />
     </>
   );
 }
--- components/about/index.ts
+++ components/about/index.ts
@@ -1,2 +1,3 @@
 export { default as HeroSection } from "./HeroSection";
 export { default as ValuesSection } from "./ValuesSection";
+export { default as Team } from "./Team";
